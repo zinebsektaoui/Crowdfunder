918fdb897bf73018631b48d6e3e3dc7579b636d2
zinebsektaoui <zinebsektaoui1@gmail.com>
2026-04-02 17:54:21 +0100

    L'ajout du middleware de validation aux routes

--- Routes/projectRoute.js
+++ Routes/projectRoute.js
@@ -2,11 +2,13 @@ const express  = require("express")
 const { create, drop, update } = require("../Controllers/projectController")
 const { authMiddleware } = require("../Middlewares/authMiddleware")
 const { roleMiddleware } = require("../Middlewares/roleMiddleware")
+const validate = require("../Middlewares/validateMiddleware")
+const projectSchema = require("../Validators/projectValidator")
 
 const router = express.Router();
 
-router.post("/", authMiddleware, roleMiddleware("Project Owner"), create)
+router.post("/", authMiddleware, roleMiddleware("Project Owner"), validate(projectSchema), create)
 router.delete("/:id", authMiddleware, roleMiddleware("Project Owner"), drop)
-router.put("/:id", authMiddleware, roleMiddleware("Project Owner"), update)
+router.put("/:id", authMiddleware, roleMiddleware("Project Owner"), validate(projectSchema), update)
 
 module.exports = router
