4bccfc53a1d878a4ecea0b430ffce8a3e2ec60c1
zinebsektaoui <zinebsektaoui1@gmail.com>
2026-04-02 17:59:52 +0100

    update

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 const express = require("express")
 const dotenv = require("dotenv")
 const mongoose = require("mongoose")
-const userRoutes = require("./Routes/userRoutes")
+const userRoutes = require("./Routes/authRoutes")
+const projectRoute = require("./Routes/projectRoute")
 
 dotenv.config()
 const app = express()
 app.use(express.json())
 
 app.use("/user", userRoutes)
+app.use("/project", projectRoute)
 
 const port = process.env.PORT
 const MONGODB_URL = process.env.MONGODB_URI
