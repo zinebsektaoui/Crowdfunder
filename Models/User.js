7dafab2326de3afd8861c960f104cc0c971bfd6e
zinebsektaoui <zinebsektaoui1@gmail.com>
2026-04-03 12:32:08 +0100

    L'ajout du balance

--- Models/User.js
+++ Models/User.js
@@ -4,6 +4,7 @@ const userSchema = new mongoose.Schema({
     "name" : String,
     "email" : String,
     "password" : String,
+    "balance" : Number,
     "role" : {
         "type" : String,
         "default" : "projectOwner",
