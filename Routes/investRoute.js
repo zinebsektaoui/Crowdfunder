b24381654632eca249a9afbb057973a6f41212b8
zineb_sektaoui <zinebsektaoui1@gmail.com>
2026-04-05 00:08:34 +0200

    implementation des routes ajouter

--- Routes/investRoute.js
+++ Routes/investRoute.js
@@ -1,4 +1,4 @@
-const { investInProject, getOpenProjects } = require("../Controllers/investmentController")
+const { investInProject, getOpenProjects, getInvestmentsDetails, getProjectDetails } = require("../Controllers/investmentController")
 const express = require("express")
 const { authMiddleware } = require("../Middlewares/authMiddleware")
 const { roleMiddleware } = require("../Middlewares/roleMiddleware")
@@ -9,5 +9,7 @@ const router = express.Router()
 
 router.post("/:projectId", authMiddleware, roleMiddleware("Investor"), validate(investSchema), investInProject)
 router.get("/", authMiddleware, roleMiddleware("Investor"), getOpenProjects)
+router.get("/getProjects/:projectId", authMiddleware, roleMiddleware("Investor"), getProjectDetails)
+router.get("/getInvestments", authMiddleware, roleMiddleware("Investor"), getInvestmentsDetails)
 
 module.exports = router
